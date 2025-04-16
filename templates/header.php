0ad6f83e19e41900e8ef436433d4e736501ef2df
Philip-11 <magsaysayphil@gmail.com>
2025-04-16 23:50:22 +0800

    Separated header and footer

---
+++ templates/header.php
@@ -0,0 +1,34 @@
+<div class="container shadow-lg rounded">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <div class="col-md-3 mb-2 mb-md-0">
+            <a href="index.php" class="d-flex align-items-center mb-3 mb-lg-0 me-md-auto fs-3 link-body-emphasis text-decoration-none">
+                <img style="width: 25px; height: 100%; margin: 10px" src="public/cow-svgrepo-com.svg" alt="A cow" srcset="">
+                <span>Mooz</span>
+            </a>
+        </div>
+
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li class="nav-item">
+                <a class="nav-link px-2 link-secondary" href="">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link px-2" href="">Features</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link px-2" href="">
+                    Pricing
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link px-2" href="">
+                    FAQ
+                </a>
+            </li>
+        </ul>
+
+        <div class="col-md-3 text-end">
+            <button class="btn btn-outline-primary me-2">Login</button>
+            <button class="btn btn-primary">Signup</button>
+        </div>
+    </header>
+</div>
